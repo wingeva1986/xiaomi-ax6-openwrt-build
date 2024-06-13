20655d647850dc6657ab4da55561d5e879815bbd
Deleted-Account <48331538+Deleted-Account@users.noreply.github.com>
2024-06-14 01:47:34 +0800

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -28,13 +28,13 @@ git clone https://github.com/immortalwrt/packages.git --single-branch --depth 1 
 git clone https://github.com/immortalwrt/immortalwrt.git --single-branch --depth 1 temp/immortalwrt
 
 ## KMS激活
-# mv temp/luci/applications/luci-app-vlmcsd package/new/luci-app-vlmcsd
-# mv temp/packages/net/vlmcsd package/new/vlmcsd
-# # edit package/new/luci-app-vlmcsd/Makefile
-# sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/new/luci-app-vlmcsd/Makefile
+mv temp/luci/applications/luci-app-vlmcsd package/new/luci-app-vlmcsd
+mv temp/packages/net/vlmcsd package/new/vlmcsd
+# edit package/new/luci-app-vlmcsd/Makefile
+sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/new/luci-app-vlmcsd/Makefile
 
-rm -rf temp/luci/applications/luci-app-vlmcsd package/new/luci-app-vlmcsd
-rm -rf temp/packages/net/vlmcsd package/new/vlmcsd
+# rm -rf temp/luci/applications/luci-app-vlmcsd package/new/luci-app-vlmcsd
+# rm -rf temp/packages/net/vlmcsd package/new/vlmcsd
 
 # AutoCore
 mv temp/immortalwrt/package/emortal/autocore package/new/autocore
